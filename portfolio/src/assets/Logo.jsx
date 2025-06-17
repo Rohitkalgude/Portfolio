d956afe4512e3ae757579777d9a4bb3b99e2c5ce
Rohit Kalgude <rohitkalgude860@gmail.com>
2025-06-17 11:38:50 +0530

    Removed unnecessary portfolio folder

--- portfolio/src/assets/Logo.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import logo from "./logo.png"
-function Logo({className}) {
-  return (
-    <img src={logo} className={className} alt="" srcset="" />
-  )
-}
-
-export default Logo
